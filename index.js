502100bf0bf223264dae475550e8b4c0d062d483
programista-nautil <programista@nautil.pl>
2025-07-29 10:34:05 +0200

    aktualizacja wersji i ignorowanie warningow

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-import "expo-router/entry";
+import { LogBox } from 'react-native'
+import 'expo-router/entry'
+
+// Ignoruj te specificzne ostrzeżenia związane z NativeEventEmitter
+LogBox.ignoreLogs([
+	'new NativeEventEmitter',
+	'`new NativeEventEmitter()` was called with a non-null argument without the required `addListener` method',
+	'`new NativeEventEmitter()` was called with a non-null argument without the required `removeListeners` method',
+])
