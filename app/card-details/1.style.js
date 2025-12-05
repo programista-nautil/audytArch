be2fe7032e54a8fc1e879407e449451ccc2ed9d7
programista-nautil <programista@nautil.pl>
2025-12-05 11:50:48 +0100

    aktualizacja zaleznosci

--- app/card-details/1.style.js
+++ app/card-details/1.style.js
@@ -1,7 +1,6 @@
 import { StyleSheet } from 'react-native'
 
 import { FONT, SIZES, COLORS } from '../../constants'
-import { getApp } from 'firebase/app'
 
 const styles = StyleSheet.create({
 	container: {
